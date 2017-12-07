598e153e10223cb0ca87735216d411ea6e8ad18b
Constantine Alpashkin <c-constantine.alpashkin@edifecs.com>
2017-12-07 16:26:28 +0300

    Revert "develop branch nullified;" Restoring branch contents
    This reverts commit e617bf196103c3c4076cf9e46af93fa002986e06.

---
+++ Network/src/TCPConnection.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by Varvara on 06.12.2017.
+ */
+public class TCPConnection {
+    private Socket socket;
+    private Thread rxThread;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private TCPConnectionEventListener eventListener;
+
+
+    public TCPConnection(Socket sckt, TCPConnectionEventListener eventListener) {
+        this.socket = sckt;
+        this.eventListener = eventListener;
+        eventListener.onConnectionEstablished(this);
+
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            System.out.println("Cant initialize streams");
+            e.printStackTrace();
+        } finally {
+
+        }
+
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String line="";
+                try {
+                    while ((line=in.readLine())!=null){
+                        eventListener.onReceiveMessage(TCPConnection.this, line);
+                    }
+                    in.close();
+                    out.close();
+                    disconnect(TCPConnection.this);
+                    eventListener.onDisconnect(TCPConnection.this);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        rxThread.start();
+    }
+
+
+
+    private void disconnect(TCPConnection tcpConnection) {
+        System.out.println("Terminating TCP connection: "+tcpConnection);
+        Thread.currentThread().interrupt();
+    }
+
+    @Override
+    public String toString() {
+        return socket.getInetAddress() + ": " + socket.getPort();
+    }
+
+    public void sendString(String str) {
+        try {
+            out.write(str);
+            out.newLine();
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
